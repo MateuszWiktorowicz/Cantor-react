b1ae2fda5f18dc906d2334c919f85c7c7a0f68ad
Mateusz Wiktorowicz <wiktorowicz.mat@gmail.com>
2021-09-01 16:17:40 +0200

    created array of currencies

---
+++ src/App/currencies.js
@@ -0,0 +1,14 @@
+export const currencies = [
+    {
+        short: "EUR",
+        name: "Euro",
+        rate: 4.5,
+
+    },
+    {
+        short: "USD",
+        name: "Dolar Amerykański",
+        rate: 4,
+
+    },
+];
